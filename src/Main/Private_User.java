cd920d1632a83161373cf270e8d71105e404c874
Matt8ac <Matt8ac@gmail.com>
2020-05-19 15:49:43 +0300

    Update Private_User.java

--- src/Main/Private_User.java
+++ src/Main/Private_User.java
@@ -2,11 +2,66 @@ package Main;
 
 public class Private_User extends User {
     public Product all_prod = new Product(false);
-    
+    private double weight;
+    private double height;
+    private String First_Name;
+    private String Last_Name;
+    private String Country;
+    private String City;
+    private double Age;
+    private int Gender;
+    private String Email;
 
-    public Private_User(String us, String ps){
+
+    public Private_User(String us /*username*/, String ps /*password*/,
+                        double weight, double height, String fname, String last_Name, String country, String city,
+                        double age, int gender, String email){
         super(us, ps);
+        this.First_Name = fname;
+        this.height = height;
+        this.weight = weight;
+        this.Last_Name = last_Name;
+        this.Age = age;
+        this.City = city;
+        this.Country = country;
+        this.Gender = gender; // 1 for a male, 0 for a female ;
+        this.Email = email;
+    }
 
+    public String Get_fname(){
+        return this.First_Name;
+    }
+
+    public String Get_lname(){
+        return this.Last_Name;
+    }
+
+    public String Get_Email(){
+        return this.Email;
+    }
+
+    public String Get_City(){
+        return this.City;
+    }
+
+    public String Get_Country(){
+        return this.Country;
+    }
+
+    public int Get_gender(){
+        return this.Gender;
+    }
+
+    public double Get_Hei(){
+        return this.height;
+    }
+
+    public double Get_Age(){
+        return this.Age;
+    }
+
+    public double Get_Weig(){
+        return this.weight;
     }
 
 }
