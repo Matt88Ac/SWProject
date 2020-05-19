1622783ea99353b6636811e30ae7e8473553220c
Matt8ac <Matt8ac@gmail.com>
2020-05-19 15:35:12 +0300

    edits

--- src/Main/Private_User.java
+++ src/Main/Private_User.java
@@ -1,5 +1,12 @@
 package Main;
 
-public class Private_User implements User {
+public class Private_User extends User {
+    public Product all_prod = new Product(false);
     
+
+    public Private_User(String us, String ps){
+        super(us, ps);
+
+    }
+
 }
--- src/Main/User.java
+++ src/Main/User.java
@@ -1,5 +1,14 @@
 package Main;
 
-public interface User {
+public abstract class User {
     public Product p = new Product(true);
+    protected String username;
+    protected String password;
+
+    public User(String us, String ps){
+        this.password = ps;
+        this.username = us;
+    }
+
+
 }
