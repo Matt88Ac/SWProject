b25078a859435cd755eb7d9c78e32dc366dc88dc
Matt8ac <Matt8ac@gmail.com>
2020-05-19 14:59:31 +0300

    Update Product.java

--- src/Main/Product.java
+++ src/Main/Product.java
@@ -9,11 +9,11 @@ public class Product {
     public ArrayList<Meds> meds;
 
     public Product(boolean private_user){
-        d_food = new ArrayList<Dry_Food>();
-        veg_food = new ArrayList<Veg>();
-        MFD_food = new ArrayList<Meat_Fish_Dai>();
-        meds = new ArrayList<Meds>();
-        j_food = new ArrayList<Junk_Food>();
+        this.d_food = new ArrayList<Dry_Food>();
+        this.veg_food = new ArrayList<Veg>();
+        this.MFD_food = new ArrayList<Meat_Fish_Dai>();
+        this.meds = new ArrayList<Meds>();
+        this.j_food = new ArrayList<Junk_Food>();
 
         if (private_user == true){
             return;
@@ -21,9 +21,17 @@ public class Product {
 
         else{
 
+
+
         }
 
     }
+
+    private void Fill_Dry_Food() {
+
+    }
+
+
 
 
 }
