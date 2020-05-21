e21bb0aafdd9c74604fd1fd12098968abc693412
Matt8ac <Matt8ac@gmail.com>
2020-05-21 14:03:20 +0300

    Update Bus_User.java

--- src/Main/Bus_User.java
+++ src/Main/Bus_User.java
@@ -39,4 +39,56 @@ public class Bus_User extends User{
          */
     }
 
+    public void Pull_Product_Out(String name, String type){
+        if (type == "Veg"){
+            for (Veg veg:this.p.veg_food) {
+                if (veg.GetName() == name){
+                    this.p.veg_food.remove(veg);
+                    return;
+                }
+            }
+        }
+
+        else if (type == "Meds"){
+            for (Meds meds:this.p.meds) {
+                if (meds.Get_Name() == name){
+                    this.p.veg_food.remove(meds);
+                    return;
+                }
+            }
+        }
+
+        else if(type == "Meat_Fish_Dai"){
+            for (Meat_Fish_Dai mfd:this.p.MFD_food) {
+                if (mfd.GetName() == name) {
+                    this.p.veg_food.remove(mfd);
+                    return;
+                }
+            }
+        }
+
+        else if (type == "Dry Food"){
+
+            for (Dry_Food dry_food:this.p.d_food) {
+                if (dry_food.GetName() == name){
+                    this.p.veg_food.remove(dry_food);
+                    return;
+                }
+            }
+
+        }
+
+        else if(type == "Junk Food"){
+            for (Junk_Food junk_food:this.p.j_food) {
+                if (junk_food.GetName() == name){
+                    this.p.veg_food.remove(junk_food);
+                    return;
+                }
+            }
+
+        }
+
+
+    }
+
 }
