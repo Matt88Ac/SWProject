09e3fb98908ed010dda187ab7966eb1a5cf3a128
Matt8ac <Matt8ac@gmail.com>
2020-05-27 18:36:20 +0300

    Update Product.java

--- src/Main/Product.java
+++ src/Main/Product.java
@@ -3,20 +3,23 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 public class Product {
-    public ArrayList<Dry_Food> d_food;
-    public ArrayList<Veg> veg_food;
-    public ArrayList<Meat_Fish_Dai> MFD_food;
-    public ArrayList<Junk_Food> j_food;
-    public ArrayList<Meds> meds;
+    public static ArrayList<Dry_Food> d_food = null;
+    public static ArrayList<Veg> veg_food = null;
+    public static ArrayList<Meat_Fish_Dai> MFD_food = null;
+    public static ArrayList<Junk_Food> j_food = null;
+    public static ArrayList<Meds> meds = null;
 
     public Product(boolean private_user){
-        this.d_food = new ArrayList<Dry_Food>();
-        this.veg_food = new ArrayList<Veg>();
-        this.MFD_food = new ArrayList<Meat_Fish_Dai>();
-        this.meds = new ArrayList<Meds>();
-        this.j_food = new ArrayList<Junk_Food>();
 
-        if (private_user == true){
+        if (d_food == null) {
+            this.d_food = new ArrayList<Dry_Food>();
+            this.veg_food = new ArrayList<Veg>();
+            this.MFD_food = new ArrayList<Meat_Fish_Dai>();
+            this.meds = new ArrayList<Meds>();
+            this.j_food = new ArrayList<Junk_Food>();
+        }
+
+        if (private_user){
             return;
         }
 
