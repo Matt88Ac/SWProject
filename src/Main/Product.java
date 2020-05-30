a331cb1849d3336bb1b89f5a2bd394d4fded4453
Matt8ac <Matt8ac@gmail.com>
2020-05-30 16:17:31 +0300

    Update Product.java

--- src/Main/Product.java
+++ src/Main/Product.java
@@ -10,14 +10,12 @@ public class Product {
     public ArrayList<Meds> meds = null;
 
     public Product(boolean private_user){
-
-        if (d_food == null) {
+        
             this.d_food = new ArrayList<Dry_Food>();
             this.veg_food = new ArrayList<Veg>();
             this.MFD_food = new ArrayList<Meat_Fish_Dai>();
             this.meds = new ArrayList<Meds>();
             this.j_food = new ArrayList<Junk_Food>();
-        }
 
         if (private_user){
             return;
