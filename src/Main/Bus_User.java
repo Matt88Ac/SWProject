ad8a9f3bd1bda3a77059d3d7612efdb7189abaca
Matt8ac <Matt8ac@gmail.com>
2020-06-23 23:59:23 +0300

    Update Bus_User.java

--- src/Main/Bus_User.java
+++ src/Main/Bus_User.java
@@ -27,69 +27,6 @@ public class Bus_User extends User{
         return this.password;
     }
 
-    public void Add_Product_To_Sell_List(){
-        Veg veg = null;
-        Meat_Fish_Dai mfd = null;
-        Dry_Food dry_food = null;
-        Junk_Food junk_food = null;
-
-
-
-        /*
-
-        Gotta add selection choices and stuff like that.
-
-
-         */
-    }
-
-  /*  public void Pull_Product_Out(String name, String type){
-        if(this.path == null){
-            return;
-        }
-
-        if (type == "Veg"){
-            for (Veg veg:this.p.veg_food) {
-                if (veg.GetName() == name){
-                    this.p.veg_food.remove(veg);
-                    return;
-                }
-            }
-        }
-
-
-        else if(type == "Meat_Fish_Dai"){
-            for (Meat_Fish_Dai mfd:this.p.MFD_food) {
-                if (mfd.GetName() == name) {
-                    this.p.veg_food.remove(mfd);
-                    return;
-                }
-            }
-        }
-
-        else if (type == "Dry Food"){
-
-            for (Dry_Food dry_food:this.p.d_food) {
-                if (dry_food.GetName() == name){
-                    this.p.veg_food.remove(dry_food);
-                    return;
-                }
-            }
-
-        }
-
-        else if(type == "Junk Food"){
-            for (Junk_Food junk_food:this.p.j_food) {
-                if (junk_food.GetName() == name){
-                    this.p.veg_food.remove(junk_food);
-                    return;
-                }
-            }
-
-        }
-
-
-    } */
 
     public String getCity() {
         return this.city;
